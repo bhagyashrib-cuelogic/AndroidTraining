364fcb45169af9097f30a959d081caad79f3d75c
Bhagyashri Gaikwad <bhagyashri.gaikwad@cuelogic.com>
2020-04-28 10:25:56 +0530

    changes made

--- Day18/ImageLoaderPicasso/app/src/main/java/com/example/picasso/MainActivity.kt
+++ Day18/ImageLoaderPicasso/app/src/main/java/com/example/picasso/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.picasso
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -12,8 +14,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-     val myImageView: ImageView = findViewById<ImageView>(R.id.imageView)
+//     val myImageView: ImageView = findViewById<ImageView>(R.id.imageView)
+//
+//        Picasso.get().load("https://i.imgur.com/H981AN7.jpg").into(myImageView)
 
-        Picasso.get().load("https://i.imgur.com/H981AN7.jpg").into(myImageView)
+
+        imageRecyclerView.layoutManager = LinearLayoutManager(this)
+        imageRecyclerView.adapter = ImageHolder()
     }
+
 }
